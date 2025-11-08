4c17327ce49113adf64036dc78ad029c6b999cbb
Scott Henderson <Hendersonscott23@gmail.com>
2025-11-08 23:01:12 +0000

    Add pelican-themes as a submodule

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ PATH = "content"
 TIMEZONE = 'Europe/London'
 DEFAULT_LANG = 'en'
 
-THEME = 'notmyidea'
+THEME = 'pelican-themes/notmyidea-cms'
 
 DEFAULT_PAGINATION = 10
 RELATIVE_URLS = True
--- publishconf.py
+++ publishconf.py
@@ -8,13 +8,16 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://brasstistics.co.uk"
+SITEURL = 'https://brasstistics.co.uk'
 RELATIVE_URLS = False
+DELETE_OUTPUT_DIRECTORY = True
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
+
+THEME = 'pelican-themes/notmyidea-cms'
 
 # Following items are often useful when publishing
 
